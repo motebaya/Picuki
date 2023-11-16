aa49bf063239d4e8f4bd170215a419e74065e060
motebaya <davinsexo722@gmail.com>
2023-11-17 01:37:18 +0700

    refactor, feat, fix: bs4 handle, add limit for dump id, fix issue (#4)

--- lib/Picuki.py
+++ lib/Picuki.py
@@ -4,10 +4,10 @@ picuki.com instagram downloader scrapper
 merge dgzr/in-picuki -> motebaya/picuki
 18.06.20223 - 4:58 PM
 """
-
 from httpx import AsyncClient
-from bs4 import BeautifulSoup
-from typing import List, Union, Dict, Tuple, Optional
+from .logger import logger
+from .Helper import Helper
+from typing import List, Optional, Union, Dict, Tuple, Any
 import re
 
 class Picuki(AsyncClient):
@@ -15,29 +15,15 @@ class Picuki(AsyncClient):
         super().__init__()
         self.base_url = "https://www.picuki.com"
         self.headers.update({
+            "Host": "www.picuki.com",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0"
         })
-        self.media_id: List[str] = []
-
-    def parse(self, string: str) -> BeautifulSoup:
-        return BeautifulSoup(
-            string, 'html.parser'
-        )
-
-    def clear_username(self, username: str) -> str:
-        """
-        sanitize instagram username:
-            length[30, lettes, dot, underscore, digits]
-        """
-        if not re.match(r"^[a-zA-Z0-9._]{1,30}$", username):
-            return re.sub(
-                r"[^a-zA-Z0-9_.]", "", username)
-        return username
 
     async def get_profile(self, username: str) -> Union[Tuple[str, Dict[str, str]], None]:
         page = await self.get("/profile/{}".format(
-            self.clear_username(username)
-        ))
+            Helper.clear_username(username)
+        ), timeout=10)
 
         """
         return if matched with regex
@@ -50,7 +36,7 @@ class Picuki(AsyncClient):
                 info.groupdict()
             )
 
-        soup = self.parse(page.text)
+        soup = Helper.parse(page.text)
         """
         do with bs4 if regex can't find it
         """
@@ -71,53 +57,80 @@ class Picuki(AsyncClient):
         """
         return None
 
-    async def get_media_id(self, page: str, logger = None) -> None:
-        soup = self.parse(page)
+    async def get_media_id(
+        self,
+        page: str,
+        limit: int,
+        result: Optional[List[str]] = []
+    ) -> List[str]:
         """
-        search all: [<scheme>://<host>/media/<id>]
+        grab media id from user profile.
+
+        :param str page: webpage output from this.get_profile
+        :param int limit: media id grabbed limit.
+        :param List[str] result: media id result, defaults to []
         """
+        soup = Helper.parse(page)
         if (media_id := re.findall(r"https?:\/\/(?:www\.)?picuki\.com\/media\/(\d+)", page)):
-            self.media_id.extend(
+            result.extend(
                 media_id
             )
-
-        """
-        do with bs4 if regex can't find it
-        """
-        if not bool(self.media_id):
+        else:
+            """
+            do with bs4 if regex can't find it and return None if both can't find it.
+            """
             if (media_id := soup.find_all('div', class_="photo")):
-                self.media_id.extend(list(map(
+                result.extend(list(map(
                     lambda x: x.attrs.get('href').split(
                         '/'
                     )[-1], media_id
                 )))
-            return
+            else:
+                return result # because nothing media id.
+        """
+        limit reached? break.
+        """
+        if result:
+            if (len(result) >= limit):
+                return result
 
         """
-        check next page: next if media_id not empty & stop if empty
+        check next page: next if media_id not empty & stop if empty.
         """
-        logger.info(f"media ID collected: {len(self.media_id)}")
-        if (next_page := soup.find('div', class_='load-more-wrapper')):
-            logger.warning("loading more page....")
-            if bool(self.media_id):
-                page = await self.get("{}".format(
-                    next_page.attrs.get('data-next')
-                ))
+        logger.info(f"media ID collected: {len(result)}")
+        if (next_page := soup.find('div', class_='load-more-wrapper') or \
+            soup.find('input', class_="pagination-next-page-input")
+            ):
+            logger.info("loading more page....")
+            page = await self.get("{}".format(
+                next_page.attrs.get('data-next') or \
+                    next_page.attrs.get('value')
+            ), timeout=10)
+            """
+            avoid exception: ssl.SSLWantReadError.
+            it could be help it for adding delay before continue to next request.
+            default: int: 3.
+            """
+            await Helper.delay(3)
+            await self.get_media_id(
+                page.text, limit, result
+            )
+        return result
 
-                await self.get_media_id(
-                    page.text, logger
-                )
-            return
-        return
+    async def get_media_content(self, media_id: str) -> Union[Dict[str, Any], None]:
+        """
+        grab media content: e.g video, photos, .etc
+        using regex at first bcs i think it more faster than bs4.
 
-    async def get_media_content(self, media_id: str) -> Optional[Dict[str, List[str]]]:
-        data: Dict[str, Dict[str, str]] = {
-            'media': {}
-        }
+        :param str media_id: single media id from this.get_media_id
+        :return Union[Dict[Any], None]: None | {}
+        """        
+        data: Dict[str, Any] = {'media': {}}
         page = await self.get("/media/{}".format(
             media_id
-        ))
+        ), timeout=10)
         page = page.text
+        soup = Helper.parse(page)
         """
         search: [username, time, caption, tags]
         """
@@ -139,31 +152,31 @@ class Picuki(AsyncClient):
                     like.groupdict()
                 )
             data.update(media_info)
-
-        """
-        do with bs4 if nothing
-        """
-        soup = self.parse(page)
-        if (info := soup.find('div', class_='single-profile-info')):
-            data.update(
-                dict(zip(
-                    ('username', 'time', 'caption', 'like_count', 'comments_count'),
-                    (info.find(class_=i).text.strip() for \
-                        i in (
-                            'single-photo-nickname', 
-                            'single-photo-time', 
-                            'single-photo-description',
-                            'icon-thumbs-up-alt',
-                            'icon-chat'
-                        ))
+        else:
+            """
+            do with bs4 if nothing
+            """
+            if (media_info := soup.find('div', class_='single-profile-info')):
+                data.update(
+                    dict(zip(
+                        ('username', 'time', 'caption', 'like_count', 'comments_count'),
+                        (media_info.find(class_=i).text.strip() for \
+                            i in (
+                                'single-photo-nickname', 
+                                'single-photo-time', 
+                                'single-photo-description',
+                                'icon-thumbs-up-alt',
+                                'icon-chat'
+                            ))
+                        )
                     )
                 )
-            )
 
         """
         search media: Dict[thumbnail, url]
+        @grab video list url
         """
-        video_list, image_list = [], []
+        videos_list, images_list = [], []
         if (html_video := re.findall(
             r"(\<video[\s\S]*?\<\/video)", page
         )):
@@ -171,17 +184,32 @@ class Picuki(AsyncClient):
                 if (video := re.search(
                     r"(?:\<video[^>]+?poster\=\"(?P<thumbnail>[^<]*?)\"[\s\S]*src\=\"(?P<url>[^<]*?))\"", vid
                 )):
-                    video_list.append(
+                    videos_list.append(
                         video.groupdict()
                     )
-            
+        else:
+            """
+            and again, do with bs4 if regex can't find it.
+            """
+            if (html_video := soup.find_all('video')):
+                for vid in html_video:
+                    video_data = {
+                        "thumbnail": vid.attrs.get('poster'),
+                        "url": vid.find('source').attrs.get('src')
+                    }
+                    if video_data not in videos_list:
+                        videos_list.append(
+                            video_data
+                        )
+
         """
         image: List[str, str]
+        @grab images/thumbnail url
         """
         if (html_image := re.findall(
             r"<img[^\"]src\=\"([^<]*?)\"", page
         )):
-            image_list.extend(
+            images_list.extend(
                 list(filter(
                     None,
                     html_image
@@ -189,7 +217,7 @@ class Picuki(AsyncClient):
             )
         else:
             if (html_image := soup.find_all('img')):
-                image_list.extend(
+                images_list.extend(
                     list(filter(
                         None,
                         map(
@@ -198,35 +226,11 @@ class Picuki(AsyncClient):
                         )
                     ))
                 )
-
-        """
-        and again, do with bs4 if regex can't find it.
-        """
-        if not bool(video_list):
-            
-            if (html_video := soup.find_all('video')):
-                for vid in html_video:
-                    video_data = {
-                        "thumbnail": vid.attrs.get('poster'),
-                        "url": vid.find('source').attrs.get('src')
-                    }
-                    if video_data not in video_list:
-                        video_list.append(
-                            video_data
-                        )
-
         data['media'].update({
-            'videos': video_list,
-            'images': image_list
+            'videos': videos_list,
+            'images': images_list
         })
             
         if len(data) > 1:
             return data
         return None
-            
-            
-        
-            
-             
-             
-             
