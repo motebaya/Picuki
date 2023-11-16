ba035c2c3236e6e34364f54e8187bd707f3bfea3
motebaya <davinsexo722@gmail.com>
2023-11-17 01:40:49 +0700

    refactor: change as module handler

--- main.py
+++ main.py
@@ -1,288 +1,164 @@
 #!/usr/bin/env python3
 """
 main.py CLI - Handler
-merge dgzr/in-picuki -> motebaya/picuki
-20.06.2023 - 8:00 PM 
+@github.com/motebaya
+update: 17.11.2023 - 8:00 PM 
 """
 
+from lib.logger import logger, col
+from typing import List, Dict, Optional, Union, Any, Tuple
+from lib.Helper import Helper
 from lib.Picuki import Picuki
-from lib.logger import logging, logger
-from argparse import ArgumentParser, RawTextHelpFormatter
-from typing import Dict, List
-from rich.console import Console
-from rich.panel import Panel
-from ssl import SSLWantReadError
-from aiohttp import ClientSession
-from pathlib import Path as PosixPath
-from humanize import naturalsize as get_natural_sizes
-import aiofiles, asyncio, os, re, random, time
-from rich.progress import (
-    Progress, 
-    SpinnerColumn, 
-    BarColumn, 
-    TextColumn, 
-    DownloadColumn, 
-    TransferSpeedColumn, 
-    TimeRemainingColumn
-)
+import random
 
-def get_valid_filename(url: str) -> str:
-    """
-    get filename from string url, exit if can't find it
-    """
-    if (basename := re.search(r"^https?\:\/\/[^<]+\/q\/(?P<filename>[^\"].*?)\|\|", url)):
-        basename = list(basename.groupdict().get('filename')[:25])
-        random.shuffle(basename)
-        basename = "".join(basename)
-        return basename
-    logger.warning(f"Cannot find spesific name from url: {url}")
-    raise ValueError(
-        "invalid url"
-    )
-    
-    
-async def _download(**kwargs: Dict[str, str]) -> None:
-    """
-    url: str = set url to download
-    output: str = set custom output base folder e.g: 
-        /media/run/disk/<username>
-    filename: str
-    """
-    url = kwargs.get('url')
-    assert url is not None, "stopped, nothing url to download!"
+class Main:
+    Instance = Picuki()
 
-    media_type = kwargs.get('type')
-    output = os.path.join(os.path.realpath(kwargs.get(
-        'output',
-        './'
-    )), kwargs.get(
-        'username',
-        'picuki_media'
-    ))
+    @staticmethod
+    async def getProfileInfo(
+        username: str,
+        savelog: Optional[bool] = True
+    ) -> Union[Tuple[str ,Dict[str, Any]], None]:
+        """get user short info by suplied username.
 
-    if not os.path.exists(output):
-        try:
-            os.mkdir(output)
-            for folder in ('images', 'videos', 'thumbnails'):
-                folder_path = os.path.join(output, folder)
-                if not os.path.exists(folder_path):
-                    os.mkdir(folder_path)
-        except PermissionError:
-            logger.warning(
-                f"Directory output: {output} is not writeable!"
-            )
-            exit(1)
-    
-    filename = os.path.join(
-        f"{output}/{media_type}",
-        get_valid_filename(
-            url
-        )
-    )
-    async with ClientSession(headers={
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-        }) as session:
-        async with session.get(url) as response:
-            extension = response.headers.get('Content-Type')
-            if not extension:
-                raise ValueError(
-                    f"Cannot get mimetype of content: {url}"
+        :param str username: username target.
+        :param bool savelog: save result info to filename, defaults to False
+        :return Dict[str, str]:return result profile info.
+        """
+        username = Helper.clear_username(username)
+        logger.info(f"Getting profile: @{username}")
+        if (profileInfo := await Main.Instance.get_profile(username)):
+            webpage, profileInfo = profileInfo
+            if savelog:
+                await Helper.saveLog(
+                    username,
+                    profileInfo,
+                    "profile.json"
                 )
+            return (webpage, profileInfo)
+        logger.error(f"Failed get info from: {col.GREEN}{username!r}{col.RESET}, make sure username is correct!")
+        exit(1)
+    
+    @staticmethod
+    async def getMediaId(
+        target: str,
+        limit: Optional[int] = 50,
+        shuffle: Optional[bool] = False,
+        saveLog: Optional[bool] = True
+    ) -> Dict[str, List[str]]:
+        """
+        get media id by suplied webpage from this.getProfileInfo (save your time) or username.
+        username IG no more than 30 length, so it will get page if suplied username.
 
-            filename = "{}.{}".format(filename, extension.split('/')[-1])
-            if not os.path.exists(filename):
-                async with aiofiles.open(filename, "wb") as f:
-                    with Progress(
-                        SpinnerColumn(speed=1.5),
-                        TextColumn("[green] Downloading..", justify="right"),
-                        BarColumn(),
-                        "[progress.percentage]{task.percentage:>3.0f}%",
-                        DownloadColumn(
-                            binary_units=False
-                        ),
-                        TransferSpeedColumn(),
-                        TimeRemainingColumn(),
-                        console=Console(),
-                        transient=True
-                    ) as progress:
-                        task = progress.add_task(
-                            "[green] Downloading..", total=int(response.headers.get('content-length', 0))
-                        )
-                        async for content in response.content.iter_chunks():
-                            await f.write(
-                                content[0]
-                            )
-                            progress.update(
-                                task, advance=len(content[0])
-                            )
-                        await f.close()
-                        progress.stop()
-                Console().print(f"[green] Completed..saved as: [blue]{filename}")
-            else:
-                Console().print(f"[green] SKipping.. [blue]{filename} [green] file exist!")
+        :param str target: username/webpage.
+        :param int limit: set, how much you want to get bulk media id post from any profile.
+            _default: 50. max: ??, min: 12 (1 page).
+        :param bool shuffle: by default media id grabbed by latest to old, set `True` if want to shuffle it!.
+        :param bool saveLog: save result grabbed media id to file or not. recomended to save it for download later.
+        :return Dict[str, List[str]]: result from media id.
+        """
+        profileInfo = None
+        if len(target) < 30:
+            target, profileInfo = await Main.Instance.get_profile(
+                Helper.clear_username(
+                    target
+                )
+            )
+            Helper.show_table(profileInfo)
 
-def show_table(data: Dict[str, str], title: str = None) -> None:
-    """
-    show profile data
-    """
-    Console().print(
-        Panel.fit(
-            '\n'.join(map(
-                lambda x: f"[bold]{x[0]}:[/bold] {x[1]}",
-                dict(sorted(
-                    data.items()
-                )).items()
-            )), 
-            title="information" if not title else title,
-            border_style="blue"
-        )
-    )
+        logger.info(f"grabbing media id with limit set: {limit}")
+        mediaID = await Main.Instance.get_media_id(target, limit)
+        if (len(mediaID) != 0):
+            if shuffle:
+                random.shuffle(mediaID)
+                logger.warning("media id has shuffled!")
 
-def calculate_result(username: str) -> None:
-    """
-    show all size and total file
-    """
-    total_size: Dict[str, int] = {
-        "total": 0,
-        "images": 0,
-        "videos": 0,
-        "thumbnails": 0
-    }
-    total: int = 0
-    fullpath = os.path.realpath(username)
-    for glob in PosixPath(fullpath).glob("*"):
-        dirs_list = list(glob.iterdir())
-        for filename in dirs_list:
-            total_size[glob.name] += filename.stat().st_size
-            total_size['total'] += filename.stat().st_size
+            logger.info(f"Total media ID grabbed: {str(len(mediaID))}")
+            mediaID = {"mediaID": mediaID, "username": Helper.clear_username(
+                profileInfo.get('username') if profileInfo else \
+                    Helper.parse(
+                        target
+                    ).find(class_="profile-name-top").text.strip()
+            )}
+            if saveLog:
+                logger.info(f"Saving result..")
+                await Helper.saveLog(
+                    mediaID.get('username'), mediaID, "mediaID.json"
+                )
+                return mediaID
+            return mediaID
+        logger.error(f"Failed when get media ID from target, make sure the target is public profile!")
+        return
+
+    async def downloadMediaContent(mediaID: Dict[str, List[str]], mediaType: str) -> None:
+        """
+        get content by media id list, then download it to local file.
+
+        :param Dict[str, List[str]] mediaID: this should from result this.getMediaId!.
+        :param str mediaType: type media want to download, e.g: [images, thumbnails, videos].
+            leave it blank if you want to download all media type.
+        """
+        username = mediaID.get('username')
+        mediaID = mediaID.get('mediaID')
+        if not mediaID or len(mediaID) == 0:
+            logger.error("ValueError: Invalid Suplied Data!")
+            exit(0)
         
-        total += len(dirs_list)
-        total_size[glob.name] = "{} / {} files".format(
-            get_natural_sizes(
-                total_size[glob.name]
-            ), str(len(dirs_list)))
-
-    total_size['total'] = "{} / {} files".format(
-        get_natural_sizes(
-            total_size['total']
-        ),
-        total
-    )
-    show_table(
-        total_size,
-        f"total size: {username}"
-    )
-
-async def _main(**kwargs):
-    """
-    main: CLI handler
-    """
-    if not kwargs.get('verbose'):
-        logging.getLogger().setLevel(logging.INFO)
-    
-    module = Picuki()
-    username = module.clear_username(
-        kwargs.get('username')
-    )
-
-    selected: List[str] = ['images', 'videos', 'thumbnails']
-    if not kwargs.get('all'):
-        for select in selected:
-            if not kwargs.get(select):
-                selected.remove(
-                    select
-                )
-
-    if (profile := await module.get_profile(username)):
-        page, result = profile
-        show_table(result)
-
-        await module.get_media_id(page, logger)
-        if len(module.media_id) != 0:
-            logger.info(f"Total Media Collected: {len(module.media_id)}")
-            logger.info(f"Selected media: {selected}, starting download..")
-            for index, media in enumerate(module.media_id, 1):
-                logger.info(f"getting content from: {media} [{index} of {len(module.media_id)}]")
-                try:
-                    if (content := await module.get_media_content(media)):
-                        _media = content.pop('media')
-                        show_table(content)
-                    
-                        if 'images' in selected:
-                            if len(_media['images']) != 0:
-                                logger.info(f"Total images collected: {len(_media.get('images'))}")
-                                for index, img in enumerate(_media.get('images'), 1):
-                                    logger.info(f"downloading images ( {index} of {len(_media.get('images'))})")
-                                    await _download(
-                                        url=img,
-                                        username=username,
-                                        type="images"
-                                    )
-                            else:
-                                logger.warning('there no images to download!')
-                        
-                        if 'videos' in selected or \
-                            'thumbnails' in selected:
-                            if len(_media.get('videos')) != 0:
-                                logger.info(f"Total videos/thumbnails Collected: {len(_media.get('videos'))}")
-                                for index, vids in enumerate(_media.get('videos'), 1):
-                                    if 'thumbnails' in selected:
-                                        logger.info(f"downloading thumbnails ( {index} of {len(_media.get('videos'))})")
-                                        await _download(
-                                            url=vids.get('thumbnail'),
-                                            username=username,
-                                            type="thumbnails"
-                                        )
-
-                                    if 'videos' in selected:
-                                        logger.info(f"downloading videos ( {index} of {len(_media.get('videos'))})")
-                                        await _download(
-                                            url=vids.get('url'),
-                                            username=username,
-                                            type="videos"
-                                        )
-                            else:
-                                logger.warning("there's no videos or thumbnails to download..")
-                        time.sleep(1)
-                    else:
-                        logger.warning(f"cannot get content from media ID: {media}")
-                except (asyncio.exceptions.CancelledError, SSLWantReadError) as e:
-                    logger.warning(f"Exception: {str(e)}, in media ID: {media}")
-                    time.sleep(1)
-                    continue
-            calculate_result(
-                username
-            )
-        else:
-            logger.warning(f"The user: {profile.get('username')} don't have any post..")
-    else:
-        logger.warning(f"cannot find user: @{username}, check again username")
-
-
-if __name__ == "__main__":
-    parser = ArgumentParser(
-        description="\t\tPicuki.com\n  [ instagram bulk profile media downloader ]\n\t    @github.com/motebaya", 
-        formatter_class=RawTextHelpFormatter
-    )
-    parser.add_argument("-u", "--username", help="spesific instagram username", metavar="")
-    parser.add_argument("-i", "--images", help="just download all images", action="store_true")
-    parser.add_argument("-v", "--videos", help="just download all videos", action="store_true")
-    parser.add_argument("-t", "--thumbnails", help="just download all videos thumbnails", action="store_true")
-    parser.add_argument("-a", "--all", help="download all media", action="store_true")
-    
-    opt = parser.add_argument_group("Optional")
-    opt.add_argument("-V", "--verbose", help="enable logger debug mode", action="store_true")
-    args = parser.parse_args()
-    
-    if args.username and \
-        any([args.images, args.videos, args.thumbnails, args.all, args.verbose]):
+        selected: List[str] = ['images', 'videos', 'thumbnails']
+        if bool(mediaType):
+            for select in selected:
+                if select != mediaType:
+                    selected.remove(
+                        select
+                    )
         try:
-            asyncio.run(_main(
-                **vars(args)
-            ))
-        except (asyncio.exceptions.CancelledError, SSLWantReadError) as e:
-            logger.warning(f"Exception: {str(e)}")
-    else:
-        parser.print_help()
+            logger.info(f"Starting download: {str(len(mediaID))} Items.")
+            for index, mediaid in enumerate(mediaID, 1):
+                logger.info(f"Processing: {mediaid}->{index} of {str(len(mediaID))}")
+                if (content := await Main.Instance.get_media_content(mediaid)):
+                    mediaContent = content.pop('media')
+                    Helper.show_table(content)
+                    targetContent = mediaContent.get(selected[0] if selected[0] != "thumbnails" else "images")
+                    if len(targetContent) != 0:
+                        logger.info(f"Downloading: {str(len(targetContent))} {selected[0]} from ID:{mediaid}")
+                        for i, cont in enumerate(targetContent, 1):
+                            logger.info(f"Downloading {selected[0]}: {i} of {str(len(targetContent))}")
+                            await Helper._downloadContent(
+                                url=cont,
+                                username=username,
+                                type=mediaType
+                            )
+                            await Helper.delay(2)
+                    else:
+                        logger.warning(f"There's no {mediaType!r} to download from: {mediaid}")
+                    """
+                    did you think this to slow? but, the important it's working as well.
+                    """
+                    await Helper.delay(3)
+                else:
+                    logger.error(f"Failed get media content from ID: {mediaid}, skipped!")
+                    await Helper.delay(1)
+                    continue
+        except EOFError:
+            pass
     
-    
+    async def execute(username: str, mediaType: str) -> None:
+        """execute bulk download directly.
+
+        :param str username: username target.
+        :param str mediaType: typemedia.
+        """
+        try:
+            # dump profile info
+            webpage, profileInfo = await Main.getProfileInfo(username)
+            Helper.show_table(profileInfo, title=f"{username!r}")
+            # dump media id
+            contentMediaID = await Main.getMediaId(webpage, int(profileInfo.get('total_posts')))
+            Helper.show_table(contentMediaID.copy())
+            mediaID = contentMediaID.get('mediaID')
+            print(mediaID, len(mediaID))
+            logger.info(f"media id retrevied: {len(mediaID)}")
+            # start download
+            await Main.downloadMediaContent(contentMediaID, mediaType)
+        except Exception as e:
+            raise Exception(str(e))
